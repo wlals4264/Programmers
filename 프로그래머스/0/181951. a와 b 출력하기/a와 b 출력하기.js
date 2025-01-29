8b49cb33c0ce1cebb692668fea9f902c8ab06f2a
bluemin <zeem_m2@naver.com>
2025-01-29 12:21:30 +0900

    [level 0] Title: a와 b 출력하기, Time: 54.75 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}).on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    console.log(`a = ${Number(input[0])}`)
+    console.log(`b = ${Number(input[1])}`);
+});
