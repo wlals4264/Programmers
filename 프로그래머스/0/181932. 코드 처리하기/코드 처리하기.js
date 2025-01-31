0eb527424791d50402ad4a34fb68a4c2ec6b7cf7
bluemin <zeem_m2@naver.com>
2025-01-31 17:10:59 +0900

    [level 0] Title: 코드 처리하기, Time: 14.44 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -1,10 +1,4 @@
 function solution(code) {
-    // 문자열 code를 읽으면서 1이 나오면 mode를 바꾼다. 초기값은 0
-    // 0일때는 code[idx]가 1이 아니고 idx가 짝수면 ret의 맨 뒤에 code[idx] 추가
-    // 1일때는 code[idx]가 1이 아니고 idx가 홀수면 ret의 맨 뒤에 code[idx] 추가
-    // 1일때는 mode를 바꾼다. 1이면 0으로, 0이면 1로
-    // return ret
-    // ret이 빈 문자열이면 "EMPTY"를 리턴
     const codeArr = Array.from(code);
     let ret = '';
     let mode = 0;
