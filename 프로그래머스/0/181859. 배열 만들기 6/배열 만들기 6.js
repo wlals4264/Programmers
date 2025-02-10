dd334f646cea1a5c2ff161eb6f7420eadd74a412
bluemin <zeem_m2@naver.com>
2025-02-10 13:00:04 +0900

    [level 0] Title: 배열 만들기 6, Time: 8.77 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+    const stk = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(stk.length === 0) stk.push(arr[i])
+        else if(stk[stk.length - 1] === arr[i]) stk.pop()
+        else stk.push(arr[i])
+    }
+    return stk.length > 0 ? stk : [-1];
+}
