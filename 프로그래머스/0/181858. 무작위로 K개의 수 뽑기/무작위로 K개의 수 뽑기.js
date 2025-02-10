818bd58af026327720f38c28ebb4f6646d918720
bluemin <zeem_m2@naver.com>
2025-02-10 13:08:15 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 5.65 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,17 @@
+function solution(arr, k) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(!result.includes(arr[i])) {
+            result.push(arr[i]); 
+        }
+        if (result.length === k) {  
+            break;
+        }
+    }
+    
+    while (result.length < k) {
+        result.push(-1);
+    }
+    
+    return result
+}
