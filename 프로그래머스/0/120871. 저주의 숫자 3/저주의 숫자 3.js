cf3cb9f723222bf66d83eea001fde3bb96fd444f
bluemin <zeem_m2@naver.com>
2025-03-14 17:57:52 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let count = 0;
+    let num = 0;
+    
+    while (count < n) {
+        num++;
+        if (num % 3 === 0 || num.toString().includes('3')) continue;
+        count++;
+    }
+    
+    return num;
+}
